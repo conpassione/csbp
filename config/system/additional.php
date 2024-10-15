a9fc4e6658666bf02a9a5a71a1fe74167f3691be
Renzo Bauen <info@conpassione.ch>
2024-10-15 23:03:20 +0200

    First commit

---
+++ config/system/additional.php
@@ -0,0 +1,67 @@
+<?php
+
+$GLOBALS['TYPO3_CONF_VARS'] = array_replace_recursive(
+    $GLOBALS['TYPO3_CONF_VARS'],
+    [
+        'DB' => [
+            'Connections' => [
+                'Default' => [
+                    'dbname' => getenv('TYPO3_DB_DBNAME'),
+                    'driver' => getenv('TYPO3_DB_DRIVER'),
+                    'host' => getenv('TYPO3_DB_HOST'),
+                    'password' => getenv('TYPO3_DB_PASSWORD'),
+                    'port' => getenv('TYPO3_DB_PORT'),
+                    'user' => getenv('TYPO3_DB_USERNAME'),
+                ],
+            ],
+        ],
+        'EXTENSIONS' => [
+            'backend' => [
+                'backendFavicon' => getenv('TYPO3_BE_FAVICON'),
+                'backendLogo' =>  getenv('TYPO3_BE_LOGO') ,
+                'loginBackgroundImage' => getenv('TYPO3_BE_LOGINBGIMAGE'),
+                'loginFootnote' => getenv('TYPO3_BE_LOGINFOOTNOTE'),
+                'loginHighlightColor' => getenv('TYPO3_BE_HIGHLIGHTCOLOR'),
+                'loginLogo' => getenv('TYPO3_BE_LOGINLOGO'),
+                'loginLogoAlt' => getenv('TYPO3_BE_LOGINLOGOALT'),
+            ],
+        ],
+        'EXTCONF' => [
+            'lang' => [
+                'availableLanguages' => [
+                    'de',
+                    'fr',
+                    'it'
+                ]
+            ]
+        ],
+
+        // This GFX configuration allows processing by installed ImageMagick 6
+        'GFX' => [
+            'processor' => 'ImageMagick',
+            'processor_path' => '/usr/bin/',
+            'processor_path_lzw' => '/usr/bin/',
+        ],
+        // This mail configuration sends all emails to mailpit
+        'MAIL' => [
+            'transport' => getenv('MAIL_TRANSPORT'),
+            'transport_sendmail_command' => getenv('MAIL_SENDMAIL_COMMAND'),
+            'transport_smtp_encrypt' => getenv('MAIL_SMTP_ENCRYPT'),
+            'transport_smtp_server' => getenv('MAIL_SMTP_SERVER'),
+            'transport_smtp_port' => getenv('MAIL_SMTP_PORT'),
+            'transport_smtp_user' => getenv('MAIL_SMTP_USER'),
+            'transport_smtp_pass' => getenv('MAIL_SMTP_PASSWORD'),
+        ],
+        'SYS' => [
+            'UTF8filesystem' => true,
+            'defaultScheme' => 'https',
+            'trustedHostsPattern' => getenv('TYPO3_SYS_HOSTPATTERN'),
+            'phpTimeZone' => 'Europe/Zurich',
+            'systemLocale' => 'de_CH.UTF-8',
+            'features' => [
+                'security.backend.htmlSanitizeRte' => true,
+                'security.usePasswordPolicyForFrontendUsers' => true,
+            ],
+        ],
+    ]
+);
---
+++ config/system/settings.php
@@ -0,0 +1,123 @@
+<?php
+return [
+    'BE' => [
+        'debug' => true,
+        'installToolPassword' => '$argon2i$v=19$m=65536,t=16,p=1$My4wN1JJbHE1SmRjdkNlRA$uZ75Gi2HDqpmDhG4GmgS3gvubN62Qi6NE/dOgz9GPZo',
+        'passwordHashing' => [
+            'className' => 'TYPO3\\CMS\\Core\\Crypto\\PasswordHashing\\Argon2iPasswordHash',
+            'options' => [],
+        ],
+    ],
+    'DB' => [
+        'Connections' => [
+            'Default' => [
+                'charset' => 'utf8mb4',
+                'dbname' => 'db',
+                'driver' => 'mysqli',
+                'host' => 'db',
+                'password' => 'db',
+                'port' => 3306,
+                'tableoptions' => [
+                    'charset' => 'utf8mb4',
+                    'collate' => 'utf8mb4_unicode_ci',
+                ],
+                'user' => 'db',
+            ],
+        ],
+    ],
+    'EXTENSIONS' => [
+        'backend' => [
+            'backendFavicon' => '',
+            'backendLogo' => 'EXT:csbp/Resources/Public/Icons/alfabeta.svg',
+            'loginBackgroundImage' => 'EXT:csbp/Resources/Public/Images/bernesealps.png',
+            'loginFootnote' => 'created by conPassione gmbh',
+            'loginHighlightColor' => '#8FA4C1',
+            'loginLogo' => 'EXT:csbp/Resources/Public/Images/logo_cp_core_G35_black_mit.png',
+            'loginLogoAlt' => 'conPassione gmbh',
+        ],
+        'extensionmanager' => [
+            'automaticInstallation' => '1',
+            'offlineMode' => '0',
+        ],
+        'redirects' => [
+            'showCheckIntegrityInfoInReports' => '1',
+            'showCheckIntegrityInfoInReportsSeconds' => '86400',
+        ],
+        'scheduler' => [
+            'maxLifetime' => '1440',
+        ],
+    ],
+    'FE' => [
+        'cacheHash' => [
+            'enforceValidation' => true,
+        ],
+        'debug' => true,
+        'disableNoCacheParameter' => true,
+        'passwordHashing' => [
+            'className' => 'TYPO3\\CMS\\Core\\Crypto\\PasswordHashing\\Argon2iPasswordHash',
+            'options' => [],
+        ],
+    ],
+    'GFX' => [
+        'processor' => 'GraphicsMagick',
+        'processor_effects' => false,
+        'processor_enabled' => true,
+        'processor_path' => '/usr/bin/',
+    ],
+    'LOG' => [
+        'TYPO3' => [
+            'CMS' => [
+                'deprecations' => [
+                    'writerConfiguration' => [
+                        'notice' => [
+                            'TYPO3\CMS\Core\Log\Writer\FileWriter' => [
+                                'disabled' => false,
+                            ],
+                        ],
+                    ],
+                ],
+            ],
+        ],
+    ],
+    'MAIL' => [
+        'transport' => 'sendmail',
+        'transport_sendmail_command' => '/usr/local/bin/mailpit sendmail -t --smtp-addr 127.0.0.1:1025',
+        'transport_smtp_encrypt' => '',
+        'transport_smtp_password' => '',
+        'transport_smtp_server' => '',
+        'transport_smtp_username' => '',
+    ],
+    'SYS' => [
+        'UTF8filesystem' => true,
+        'caching' => [
+            'cacheConfigurations' => [
+                'hash' => [
+                    'backend' => 'TYPO3\\CMS\\Core\\Cache\\Backend\\Typo3DatabaseBackend',
+                ],
+                'pages' => [
+                    'backend' => 'TYPO3\\CMS\\Core\\Cache\\Backend\\Typo3DatabaseBackend',
+                    'options' => [
+                        'compression' => true,
+                    ],
+                ],
+                'rootline' => [
+                    'backend' => 'TYPO3\\CMS\\Core\\Cache\\Backend\\Typo3DatabaseBackend',
+                    'options' => [
+                        'compression' => true,
+                    ],
+                ],
+            ],
+        ],
+        'devIPmask' => '*',
+        'displayErrors' => 1,
+        'encryptionKey' => '3f9d4c3191ae19391c14cb47c41ef92919b76e4c5b4a7506330b042ab96b3d456133184ed93cc7ef2a25a663993d221a',
+        'exceptionalErrors' => 12290,
+        'features' => [
+            'frontend.cache.autoTagging' => true,
+        ],
+        'sitename' => 'CPdev-csbp',
+        'systemMaintainers' => [
+            1,
+        ],
+    ],
+];
